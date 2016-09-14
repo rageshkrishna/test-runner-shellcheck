c6301985e0bdcffcc228f59bb9fae2cc76c745a1
Ragesh Krishna <ragesh@rageshkrishna.com>
2016-09-14 14:10:09 +0530

    Update runnerfile.sh

--- runnerfile.sh
+++ runnerfile.sh
@@ -1,5 +1,6 @@
 task_test() {
   echo "deb http://archive.ubuntu.com/ubuntu/ trusty-backports restricted main universe" >> sources.list
+  sudo apt-get clean
   sudo apt-get update
   sudo apt-get install shellcheck
   shellcheck bad.sh

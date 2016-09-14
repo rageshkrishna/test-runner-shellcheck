a7d1a7c03bb5b63a82538b9bb2db5189498ed7a4
Ragesh Krishna <ragesh@rageshkrishna.com>
2016-09-14 14:11:32 +0530

    Update runnerfile.sh

--- runnerfile.sh
+++ runnerfile.sh
@@ -1,4 +1,5 @@
 task_test() {
+  sudo rm /var/lib/apt/lists/* -vf
   echo "deb http://archive.ubuntu.com/ubuntu/ trusty-backports restricted main universe" >> sources.list
   sudo apt-get clean
   sudo apt-get update
